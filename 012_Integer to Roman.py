ec2419d9f28c885f36e7dc4138c61022a912d8be
jinhw1989 <jinhw1989@gmail.com>
2014-07-20 14:21:03 -0400

    12 Roman number

--- 012_Integer to Roman.py
+++ 012_Integer to Roman.py
@@ -1,1 +1,23 @@
+# wiki: Roman Number: http://en.wikipedia.org/wiki/Roman_numerals
 class Solution:
+    # @return a string
+
+    def intToRoman(self, num):
+        result = ""
+        transTable = {
+            1: "I", 5: "V", 10: "X", 50: "L", 100: "C", 500: "D", 1000: "M"}
+        for trans in sorted(transTable.keys())[::-1]:
+            while num != 0 and num >= trans:
+                num -= trans
+                result += transTable[trans]
+            if num == 0:
+                break
+
+        simplifyTable = {"DCCCC": "CM", "CCCC": "CD", "LXXXX":
+                         "XC", "XXXX": "XL", "VIIII": "IX", "IIII": "IV"}
+        simplifyContent = ["DCCCC", "CCCC", "LXXXX", "XXXX", "VIIII", "IIII"]
+        for item in simplifyContent:
+            if item in result:
+                result = result.replace(item, simplifyTable[item])
+
+        return result
