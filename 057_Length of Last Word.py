c3ec6cf8b38ef15f5c3127f9cd8f7e2023619869
Hongwei Jin <jinhw1989@gmail.com>
2014-07-23 19:07:06 -0400

    New attemped

--- 057_Length of Last Word.py
+++ 057_Length of Last Word.py
@@ -1,1 +1,6 @@
 class Solution:
+    # @param s, a string
+    # @return an integer
+
+    def lengthOfLastWord(self, s):
+        return len(s.strip().split(" ")[-1])
--- 136_Single Number.py
+++ 136_Single Number.py
@@ -1,1 +1,6 @@
 class Solution:
+    # @param A, a list of integer
+    # @return an integer
+
+    def singleNumber(self, A):
+        return reduce(operator.xor, A)
