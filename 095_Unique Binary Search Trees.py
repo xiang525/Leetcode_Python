bf71402616d3bf979d4c979df0ee55cea498b158
Hongwei Jin <jinhw1989@gmail.com>
2014-08-16 12:13:59 -0500

    Three attempts

--- 095_Unique Binary Search Trees.py
+++ 095_Unique Binary Search Trees.py
@@ -1,1 +1,18 @@
 class Solution:
+    # @return an integer
+
+    def numTrees(self, n):
+        # numOfTrees[i] == j means for n is i, there are j unique
+        # binary search trees for it.
+        numOfTrees = [1, 1, 2]
+        nextTrying = len(numOfTrees)
+        while nextTrying <= n:
+            # Comptute how many unique binary search trees are there for
+            # n == nextTrying.
+            count = 0
+            for center in xrange(nextTrying):
+                count += numOfTrees[center] * \
+                    numOfTrees[nextTrying - center - 1]
+            numOfTrees.append(count)
+            nextTrying += 1
+        return numOfTrees[n]
--- 141_Linked List Cycle.py
+++ 141_Linked List Cycle.py
@@ -1,1 +1,19 @@
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+
 class Solution:
+    # @param head, a ListNode
+    # @return a boolean
+
+    def hasCycle(self, head):
+        fastP = head
+        slowP = head
+        while fastP != None and fastP.next != None:
+            fastP = fastP.next.next
+            slowP = slowP.next
+            if fastP == slowP:
+                return True
+        return False
