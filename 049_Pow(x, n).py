268f0e8d28075590041d4febfb803937bceb4bf8
Hongwei Jin <jinhw1989@gmail.com>
2014-09-08 16:49:34 -0500

    40+ solutions.

--- 049_Pow(x, n).py
+++ 049_Pow(x, n).py
@@ -1,1 +1,14 @@
 class Solution:
+    # @param x, a float
+    # @param n, a integer
+    # @return a float
+
+    def pow(self, x, n):
+        if n == 0:
+            return 1
+        elif n == 1:
+            return x
+        elif n % 2:
+            return pow(x * x, n / 2) * x
+        else:
+            return pow(x * x, n / 2)
