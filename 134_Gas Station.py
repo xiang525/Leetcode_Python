6372cf7848d508e9cc1e358a19fd1b446f7a917d
Hongwei Jin <jinhw1989@gmail.com>
2014-09-08 00:55:35 -0500

    131+ solutions.

--- 134_Gas Station.py
+++ 134_Gas Station.py
@@ -1,1 +1,18 @@
 class Solution:
+    # @param gas, a list of integers
+    # @param cost, a list of integers
+    # @return an integer
+
+    def canCompleteCircuit(self, gas, cost):
+        if sum(gas) < sum(cost):
+            return -1
+        n = len(gas)
+        diff = 0
+        stationIndex = 0
+        for i in range(n):
+            if gas[i] + diff < cost[i]:
+                stationIndex = i + 1
+                diff = 0
+            else:
+                diff += gas[i] - cost[i]
+        return stationIndex
--- 137_Single Number II.py
+++ 137_Single Number II.py
@@ -1,1 +1,15 @@
 class Solution:
+    # @param A, a list of integer
+    # @return an integer
+
+    def singleNumber(self, A):
+        one = 0
+        two = 0
+        three = 0
+        for i in range(len(A)):
+            two |= A[i] & one  
+            one = A[i] ^ one  
+            three = ~(one & two) 
+            one &= three
+            two &= three
+        return one
