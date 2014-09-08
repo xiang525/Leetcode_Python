8c188754164160c150c91b3886295da6b319965a
Hongwei Jin <jinhw1989@gmail.com>
2014-09-08 15:30:57 -0500

    100+ solutions.

--- 099_Recover Binary Search Tree.py
+++ 099_Recover Binary Search Tree.py
@@ -1,1 +1,22 @@
+# Definition for a  binary tree node
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
 class Solution:
+    # @param root, a tree node
+    # @return a tree node
+    def inorder(self, root, list, listp):
+        if root:
+            self.inorder(root.left, list, listp)
+            list.append(root.val); listp.append(root)
+            self.inorder(root.right, list, listp)
+    def recoverTree(self, root):
+        list = []; listp = []
+        self.inorder(root, list, listp)
+        list.sort()
+        for i in range(len(list)):
+            listp[i].val = list[i]
+        return root
--- 106_Construct Binary Tree from Inorder and Postorder Traversal.py
+++ 106_Construct Binary Tree from Inorder and Postorder Traversal.py
@@ -1,1 +1,23 @@
+# Definition for a  binary tree node
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
 class Solution:
+    # @param inorder, a list of integers
+    # @param postorder, a list of integers
+    # @return a tree node
+
+    def buildTree(self, inorder, postorder):
+        if len(inorder) == 0:
+            return None
+        if len(inorder) == 1:
+            return TreeNode(inorder[0])
+        root = TreeNode(postorder[len(postorder) - 1])
+        index = inorder.index(postorder[len(postorder) - 1])
+        root.left = self.buildTree(inorder[0: index], postorder[0: index])
+        root.right = self.buildTree(inorder[index + 1: len(inorder)], postorder[index: len(postorder) - 1])
+        return root
