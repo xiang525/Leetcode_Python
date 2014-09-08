af0841c46aa558a5b7ae6dbe1bf6bd7bc2fcd540
Hongwei Jin <jinhw1989@gmail.com>
2014-09-08 16:08:39 -0500

    80+ solutions.

--- 081_Search in Rotated Sorted Array II.py
+++ 081_Search in Rotated Sorted Array II.py
@@ -1,1 +1,26 @@
 class Solution:
+    # @param A a list of integers
+    # @param target an integer
+    # @return a boolean
+
+    def search(self, A, target):
+        left = 0
+        right = len(A) - 1
+        while left <= right:
+            mid = (left + right) / 2
+            if A[mid] == target:
+                return True
+            if A[left] == A[mid] == A[right]:
+                left += 1
+                right -= 1
+            elif A[left] <= A[mid]:
+                if A[left] <= target < A[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            else:
+                if A[mid] <= target < A[left]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+        return False
