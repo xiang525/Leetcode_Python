3f5e7b13d19857f5c4baa4c943b31ad13760f17c
Hongwei Jin <jinhw1989@gmail.com>
2015-01-17 22:18:46 -0600

    178 180 sql

---
+++ Solutions/178_Rank Scores.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+SELECT Score, Rank FROM(
+  SELECT    Score,
+            @curRank := @curRank + IF(@prevScore = Score, 0, 1) AS Rank,
+            @prevScore := Score
+  FROM      Scores s, (SELECT @curRank := 0) r, (SELECT @prevScore := NULL) p
+  ORDER BY  Score DESC
+) t;
