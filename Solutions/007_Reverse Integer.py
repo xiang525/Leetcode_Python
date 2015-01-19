6f1a7574ce4f1bb48cb7d0dcf42490ac5aea7a01
Hongwei Jin <jinhw1989@gmail.com>
2015-01-19 12:17:49 -0600

    update 7 with overflow handler

--- Solutions/007_Reverse Integer.py
+++ Solutions/007_Reverse Integer.py
@@ -4,4 +4,7 @@ class Solution:
     def reverse(self, x):
         tmp = str(x)
         tmp = "-" + tmp[:0:-1] if tmp.startswith("-") else tmp[::-1]
-        return int(tmp)
+        if int(tmp) > 2 ** 31 - 1 or int(tmp) < -2 ** 31 + 1:
+            return 0
+        else:
+            return int(tmp)
