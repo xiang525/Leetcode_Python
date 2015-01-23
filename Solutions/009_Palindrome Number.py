0a2e0d3e9ddaf9b8750a9b8f6384c789f304574e
Hongwei Jin <jinhw1989@gmail.com>
2015-01-22 23:42:50 -0600

    Optimized Palindrome solution

--- Solutions/009_Palindrome Number.py
+++ Solutions/009_Palindrome Number.py
@@ -2,8 +2,24 @@ class Solution:
     # @return a boolean
 
     def isPalindrome(self, x):
-        if str(x).startswith("-"):
+        if x < 0:
             return False
-        y = str(x)[::-1]
-        res = True if y == y[::-1] else False
-        return res
+        divider = 1
+        while(x / divider >= 10):
+            divider *= 10
+        while(x != 0):
+            left = x / divider
+            right = x % 10
+            if left != right:
+                return False
+            x %= divider
+            x /= 10
+            divider /= 100
+        return True
+
+    # def isPalindrome(self, x):
+    #     if str(x).startswith("-"):
+    #         return False
+    #     y = str(x)[::-1]
+    #     res = True if y == y[::-1] else False
+    #     return res
