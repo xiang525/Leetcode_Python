83caff01829e3c9913193a4bc2f9aeb9e0fbcbed
Hongwei Jin <jinhw1989@gmail.com>
2015-01-30 21:43:37 -0600

    Improved solution 14

--- Hints/014_Longest Common Prefix.md
+++ Hints/014_Longest Common Prefix.md
@@ -1,9 +1,14 @@
 ### Problem:
- >
+Write a function to find the longest common prefix string amongst an array of strings.
 
 ### Hints:
+Assumptions:  
+1. no spaces;  
+2. two functions like `min` and `max` of string list will retune the minimum and maximum string;
+3. instead of comparing all strings, compare min and max instead;
+
 
 #### References:
 
 #### Tags:
-
+String
--- Solutions/014_Longest Common Prefix.py
+++ Solutions/014_Longest Common Prefix.py
@@ -1,12 +1,25 @@
 class Solution:
 
     def longestCommonPrefix(self, strs):
-        if len(strs) == 0:
-            return ""
-        longest = strs[0]
-        for str in strs[1:]:
-            i = 0
-            while i < len(str) and i < len(longest) and str[i] == longest[i]:
-                i += 1
-            longest = longest[:i]
-        return longest
+        "Given a list of pathnames, returns the longest common leading component"
+        if not strs:
+            return ''
+        s1 = min(strs)
+        s2 = max(strs)
+        print s1, s2
+        for i, c in enumerate(s1):
+            if c != s2[i]:
+                return s1[:i]
+        return s1
+
+
+    # def longestCommonPrefix(self, strs):
+    #     if len(strs) == 0:
+    #         return ""
+    #     longest = strs[0]
+    #     for str in strs[1:]:
+    #         i = 0
+    #         while i < len(str) and i < len(longest) and str[i] == longest[i]:
+    #             i += 1
+    #         longest = longest[:i]
+    #     return longest
