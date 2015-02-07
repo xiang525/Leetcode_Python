d703b5131c3aceec72448699331eb7e29b88a3df
jinhw1989 <jinhw1989@gmail.com>
2015-02-07 17:15:40 -0600

    update 20

--- Hints/020_Valid Parentheses.md
+++ Hints/020_Valid Parentheses.md
@@ -1,9 +1,16 @@
 ### Problem:
- >
+Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+The brackets must close in the correct order, "()" and "()[]{}" are all valid but "(]" and "([)]" are not.
 
 ### Hints:
+Stack to push in and pop out to match brackets.
+1. define dict of brackets, one trick to define them like {")":"("}  
+2. like a compiler push into stack if not in keys  
+3. pop out when it is a key, and match them together  
+4. check the size of stack at the end of processing.  
 
 #### References:
 
 #### Tags:
-
+Stack String
--- Solutions/020_Valid Parentheses.py
+++ Solutions/020_Valid Parentheses.py
@@ -10,3 +10,23 @@ class Solution:
                 if len(stack) == 0 or stack.pop() != lookup[i]:
                     return False
         return len(stack) == 0
+
+    """
+    naive way
+    """
+    # def isValid(self, s):
+    # # match = [("(",")"),("[","]"),("{","}")]
+    # match = {")": "(", "]": "[", "}": "{"}
+    # stack = []
+    # for c in s:
+    #     if c not in match:
+    #         stack.append(c)
+    #     else:
+    #         if len(stack) == 0:
+    #             return False
+    #         elif stack.pop() != match[c]:
+    #             return False
+    # if len(stack) == 0:
+    #     return True
+    # else:
+    #     return False
